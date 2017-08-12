3bb12a6ac750107673cb2b062789df7f114b769e
pyk <x24870@gmail.com>
2017-08-12 23:12:40 +0800

    add other files

--- main.py
+++ main.py
@@ -1,67 +1,83 @@
 # -*- coding: utf-8 -*-
 
 from matplotlib import pyplot as plt
+import numpy as np
 
-def getZ(string):
-    string = string.split(' ')
-    return string
+import ProcessPly as pp
+import GridMap
 
-def readPly(filename):
-    source = open(filename, 'r')
-    target = open('target.txt', 'w')
-    
-    header = []
-    zPoints = []
-    points = []
-    content = source.readlines()
-    
-    foundHeaderEnd = False
-    for item in content:
-        if not foundHeaderEnd:
-            header.append(item)
-            if item == "end_header\n":
-                foundHeaderEnd = True
-        else:
-            value = getZ(item)
-            zPoints.append(float(value[2]))
-            if float(value[2]) > -1.50 and float(value[2]) < -1.0:
-                points.append(item)
-        
-    #modify header
-    for idx in range(len(header)):
-        if header[idx].find('vertex') >= 0:
-            elements = header[idx].split(' ')
-            elements[2] = str(len(points)) + '\n'
-            header[idx] = elements[0] + ' ' + elements[1] + ' ' + elements[2]
-    
-    #write header
-    for item in header:
-        target.write(item)
-    #write points
-    for item in points:
-        target.write(item)
-    
-    source.close()
-    target.close()
-    
-    return zPoints
+###test class
+point_cloud = [[0, 0], [0, 1], [1, 0], [0, -1], [-1, 0], [0.3, -0.3], [-1.6, 0.6]]
+
+gMap = GridMap.GridMap(point_cloud)
+
+print(gMap)
+
+#x = []
+#y = []
+#for point in point_cloud:
+#    x.append(point[0])
+#    y.append(point[1])
+#   
+#plt.figure(figsize=(8, 8))
+#plt.scatter(x, y)
 
 
-### Main ###
-filename = "file.txt"
-zPoints = readPly(filename)
 
-#plot
-x = zPoints
 
-# the histogram of the data
-plt.hist(x, 50, normed=0, facecolor='green', alpha=0.75)
 
-plt.xlabel('Value')
-plt.ylabel('Count')
-plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$')
-plt.axis([-2, 2, 0, 100000])
-plt.grid(True)
+###show point cloud
+#filename = "target.txt"
+#source = open(filename, 'r')
+#
+#points = []
+#content = source.readlines()
+#
+#foundHeaderEnd = False
+#for item in content:
+#    if not foundHeaderEnd:
+#        if item == "end_header\n":
+#            foundHeaderEnd = True
+#    else:
+#        values = item.split(' ')
+#        if len(values) < 2:
+#            break
+#        x = float(values[0])
+#        y = float(values[1])
+#        points.append([x, y])
+#        
+#print(points)
+#print(len(points))
+#
+##plot
+#plt.figure(figsize=(8,12))
+#x = []
+#y = []
+#for point in points:
+#    x.append(point[0])
+#    y.append(point[1])
+#    
+#plt.scatter(x, y, s=0.1)
+#plt.show()
 
-plt.show()
+
+
+###process ply file
+#filename = "file.txt"
+#zPoints = pp.readPly(filename)
+#
+##plot
+#x = zPoints
+#
+## the histogram of the data
+#plt.hist(x, 50, normed=0, facecolor='green', alpha=0.75)
+#
+#plt.xlabel('Value')
+#plt.ylabel('Count')
+#plt.title("Distribute of Z values")
+#plt.axis([-2, 2, 0, 100000])
+#plt.grid(True)
+#
+#plt.show()
+
 
